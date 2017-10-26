43f8c131af04f6b9d7023c54c3b93445dfc453a8
jaketrent <trent.jake@gmail.com>
2017-10-26 07:21:10 -0600

    fn returns maybe number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-console.log('starting...')
+// @flow
 
-console.log('done.')
+const rand = _ => Math.random()
+const isOk = num => num > 0.5
+
+const calcMaybeNull = (): ?number => {
+  const n = rand()
+  return isOk(n) ? n : null
+}
+
+const printMaybeNull = (num: ?number) =>
+  num ? console.log('above 0.5', num) : console.log('was null')
+
+const main = _ => {
+  console.log('starting...')
+
+  printMaybeNull(calcMaybeNull())
+
+  console.log('done.')
+}
+
+main()
