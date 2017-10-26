be7a80d2403361148e65c41a53c3a3b6bffa6a85
jaketrent <trent.jake@gmail.com>
2017-10-26 07:29:16 -0600

    result as tuple example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const rand = _ => Math.random()
 const isOk = num => num > 0.5
 
+// NULLABLE example
+
 const calcMaybeNull = (): ?number => {
   const n = rand()
   return isOk(n) ? n : null
@@ -11,10 +13,31 @@ const calcMaybeNull = (): ?number => {
 const printMaybeNull = (num: ?number) =>
   num ? console.log('above 0.5', num) : console.log('was null')
 
+// RESULT example
+
+type Ok = [true, number]
+type Err = [false, Error]
+type Result = Ok | Err
+
+const calcResult = (): Result => {
+  try {
+    const n = rand()
+    if (isOk(n)) return [true, n]
+    else throw new Error('Not above 0.5')
+  } catch (err) {
+    return [false, err]
+  }
+}
+
+const printResult = (result: Result) =>
+  result[0] ? console.log('above 0.5', result[1]) : console.log(result[1])
+
 const main = _ => {
   console.log('starting...')
 
   printMaybeNull(calcMaybeNull())
+
+  printResult(calcResult())
 
   console.log('done.')
 }
